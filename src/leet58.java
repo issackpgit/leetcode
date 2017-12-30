af81360986fedefe03eceabbbd5293f5e993463d
issackoshypanicker <issackoshypanicker@192.168.0.13>
2017-12-30 18:55:40 -0500

    Initial

---
+++ src/leet58.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+class Solution58 {
+    public int lengthOfLastWord(String s) {
+    	
+    	  String[] arr = s.split(" ");    
+      
+    	  if(arr.length == 0) return 0;
+
+      int n = arr.length;
+      int no = arr[n-1].length();
+      	return no; 
+    }
+}
+
+public class leet58 {
+    public static String stringToString(String input) {
+        assert input.length() >= 2;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < input.length() - 1; i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == '\\') {
+                char nextChar = input.charAt(i+1);
+                switch (nextChar) {
+                    case '\"': sb.append('\"'); break;
+                    case '/' : sb.append('/'); break;
+                    case '\\': sb.append('\\'); break;
+                    case 'b' : sb.append('\b'); break;
+                    case 'f' : sb.append('\f'); break;
+                    case 'r' : sb.append('\r'); break;
+                    case 'n' : sb.append('\n'); break;
+                    case 't' : sb.append('\t'); break;
+                    default: break;
+                }
+                i++;
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+            
+            int ret = new Solution58().lengthOfLastWord(s);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
