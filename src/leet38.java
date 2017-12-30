fffdd304df9c4dd4185aa1507d23ada2befa551d
issackoshypanicker <issackoshypanicker@192.168.0.13>
2017-12-30 02:03:05 -0500

    Initial

---
+++ src/leet38.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class Solution38 {
+    public String countAndSay(int n) {
+        
+    	return null;
+    }
+}
+
+public class leet38 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            
+            String ret = new Solution38().countAndSay(n);
+            
+            String out = (ret);
+            
+            System.out.print(out);
+        }
+    }
+}
