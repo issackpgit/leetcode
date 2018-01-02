ff8798150b535217710b997359e58bbcd54d50d9
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-01 20:55:32 -0500

    Final

--- src/leet69.java
+++ src/leet69.java
@@ -1,8 +1,43 @@
+//Author : Issac Koshy Panicker
+// 01/01/2018
+
 import java.io.*;
 
 class Solution69 {
     public int mySqrt(int x) {
-        return 0;
+    	
+//    	if(x == 1) return 1;
+//    	if(x==0) return 0;
+//    	
+//    	long r =x; 
+//    	int i = 0;
+//    	while(i<=r/2) {
+//    		if(i*i>r)
+//    			break;
+//    		else
+//    			i++;
+//    	}
+//        return i-1;
+    	
+    	 if (x == 0 || x == 1)
+             return x;
+    	     	
+    	int start = 1; int end =x/2; int ans = 0;
+    	while(start<=end) {
+    		int mid = (start+end)/2;
+    		
+    		if(mid==x/mid)
+    			return mid;
+    		
+    		if(mid<x/mid) {
+    			start = mid+1;
+    			ans = mid;
+    		}
+    		else if(mid  >x/mid) {
+    			end = mid-1;
+    		}
+    	}
+    	return ans;
     }
 }
 
