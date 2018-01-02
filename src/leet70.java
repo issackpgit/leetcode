b1fedd7d87b20058990d58024b6e3ee50e9f3ad7
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-01 22:35:51 -0500

    Final

--- src/leet70.java
+++ src/leet70.java
@@ -9,18 +9,29 @@ class Solution70 {
     
     	if(n==0) return 0;
     	n++;
-    	int f[] = new int[n+1];
-        int i;
-          
-        f[0] = 0;
-        f[1] = 1;
-         
-        for (i = 2; i <= n; i++)
-        {
-            f[i] = f[i-1] + f[i-2];
-        }
-          
-    	return f[n];
+    	
+//    	int f[] = new int[n+1];
+//        int i;
+//          
+//        f[0] = 0;
+//        f[1] = 1;
+//         
+//        for (i = 2; i <= n; i++)
+//        {
+//            f[i] = f[i-1] + f[i-2];
+//        }
+//          
+//    	return f[n];
+    
+     int a = 0, b = 1, c;
+           
+     for (int i = 2; i <= n; i++)
+       {
+    	 	c = a + b;
+        a = b;
+        b = c;
+       }
+     return b;        
         
     }
 }
