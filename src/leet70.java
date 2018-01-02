4f70145a67da7d083feee96b7090680688387a13
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-01 22:31:08 -0500

    Final

---
+++ src/leet70.java
@@ -0,0 +1,42 @@
+//Author : Issac Koshy Panicker
+//01/01/2018
+
+import java.io.*;
+
+class Solution70 {
+    public int climbStairs(int n) {
+    	
+    
+    	if(n==0) return 0;
+    	n++;
+    	int f[] = new int[n+1];
+        int i;
+          
+        f[0] = 0;
+        f[1] = 1;
+         
+        for (i = 2; i <= n; i++)
+        {
+            f[i] = f[i-1] + f[i-2];
+        }
+          
+    	return f[n];
+        
+    }
+}
+
+public class leet70 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            
+            int ret = new Solution70().climbStairs(n);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
