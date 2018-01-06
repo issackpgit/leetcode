a4bcaa297c639a0dd821acb24961bf2ef7a301c9
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-05 19:14:15 -0500

    Final

--- src/leet122.java
+++ src/leet122.java
@@ -1,9 +1,30 @@
+//Author : Issac Koshy Panicker
+//05-01-2018
+
 import java.io.*;
 
 class Solution122 {
     public int maxProfit(int[] prices) {
         
-    	return 0 ;
+    	if(prices.length==0) return 0;
+    	
+    	int buy = prices[0];
+    	int sell = 0,p=0;
+    	
+    	for(int i = 0 ;i<prices.length-1;i++) {
+    		if(prices[i]>prices[i+1]) {
+    			sell = prices[i];
+    			p += sell - buy;
+    			buy = prices[i+1];
+    		}
+    		if(i==prices.length-2 && prices[prices.length-1]>buy) 
+    		{	sell=prices[prices.length-1];
+    			p += sell - buy;	
+    		}
+    		
+    		
+    	}
+    	return p ;
     }
 }
 
