22fb31d75d48e66a977cf031789e8258db7172ba
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-05 19:16:32 -0500

    Initial

---
+++ src/leet169.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+class Solution169 {
+    public int majorityElement(int[] nums) {
+        
+    	
+    	return 0;
+    }
+}
+
+public class leet169 {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] nums = stringToIntegerArray(line);
+            
+            int ret = new Solution169().majorityElement(nums);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
