10c36be2b53981117c4ce0862714bea6dab63bb2
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-06 19:18:46 -0500

    Inital

--- src/leet169.java
+++ src/leet169.java
@@ -1,9 +1,24 @@
 import java.io.*;
+import java.util.*;
 
 class Solution169 {
     public int majorityElement(int[] nums) {
         
+    	int n = nums.length/2;
     	
+    	Map<Integer, Integer> map= new HashMap<Integer,Integer>();
+    	
+    	for(int i = 0;i<nums.length;i++) {
+    		if(map.containsKey(nums[i])) {
+    			map.put(nums[i], map.get(nums[i])+1);
+    			if(map.get(nums[i])>n) {
+    				return nums[i];
+    			}
+    		}
+    			else
+    			map.put(nums[i], 1);
+    		
+    	}
     	return 0;
     }
 }
