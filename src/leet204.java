d90a6fc05b2d156c9d3e3147c08dd375d1269365
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-10 18:28:30 -0500

    Final

--- src/leet204.java
+++ src/leet204.java
@@ -5,17 +5,18 @@ import java.io.*;
 
 class Solution204 {
     public int countPrimes(int n) {
-    	 boolean[] notPrime = new boolean[n];
-         int count = 0;
-         for (int i = 2; i < n; i++) {
-             if (notPrime[i] == false) {
-                 count++;
-                 for (int j = 2; i*j < n; j++) {
-                     notPrime[i*j] = true;
-                 }
-             }
-         }
-         
+    	
+    	boolean[] a =new boolean[n];
+    	int count =0;
+    	for(int i=2;i<n;i++){
+    		if(!a[i]) {
+    			count++;
+    			for(int j = 2;i*j<n;j++) {
+    				a[i*j]=true;
+    			}
+    		}
+    	}
+    	
          return count;
          
 //int[] a= new int[n-2];
