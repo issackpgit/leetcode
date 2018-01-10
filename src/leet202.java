b6fe396b5fc596af37ae6af8d301e0a209c64bf5
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-10 15:38:44 -0500

    Final

--- src/leet202.java
+++ src/leet202.java
@@ -1,11 +1,51 @@
 import java.io.*;
+import java.util.*;
 
 class Solution202 {
     public boolean isHappy(int n) {
-     
+   
+//    	    int slow, fast;
+//    	    slow = fast = n;
+//    	    do {
+//    	        slow = digitSquareSum(slow);
+//    	        fast = digitSquareSum(fast);
+//    	        fast = digitSquareSum(fast);
+//    	    } while(slow != fast);
+//    	    
+//    	    if (slow == 1) return true;
+//    	    else return false;
+    	
+    	Set<Integer> set = new HashSet<Integer>();
+   	
+    	int temp = 0,s=0; 
+    	while(set.add(n)) {
+	    	while(n!=0) {
+	    		int a = n%10;
+	    		s+= a*a;
+	    		n = n/10;
+	    		temp =s;
+	    	}
+	    	if(s == 1) {
+	    		return true;
+	    	}
+	    	else {
+	    		n = temp;
+	    		s=0;
+	    	}
+	  		
+    	}
     	
     	return false;
     }
+//    public int digitSquareSum(int n) {
+//	    int sum = 0, tmp;
+//	    while (n>0) {
+//	        tmp = n % 10;
+//	        sum += tmp * tmp;
+//	        n /= 10;
+//	    }
+//	    return sum;
+//	}
 }
 
 public class leet202 {
