552a7402be4f74e306dd95deaa13c9ecb1dcec8b
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-10 22:10:19 -0500

    Final

--- src/leet205.java
+++ src/leet205.java
@@ -1,11 +1,29 @@
+//Author : Issac Koshy Panicker
+//10-01-2018
+
 import java.io.*;
 import java.util.*;
 
 class Solution205 {
     public boolean isIsomorphic(String s, String t) {
-        
     	
-    	return false;
+    	Map<Character, Character> map = new HashMap<Character, Character>();
+        
+    	for(int i =0;i<s.length();i++) {
+    		if(map.containsKey(s.charAt(i))) {
+    			if(map.get(s.charAt(i))==t.charAt(i)) {
+    				continue;
+    			}
+    			else return false;
+    		}
+    		for(char j : map.keySet()) {
+    			if(map.get(j)==t.charAt(i)) return false;
+    		}
+    		map.put(s.charAt(i), t.charAt(i));
+    		
+    	}
+    	
+    	return true;
     }
 }
 
