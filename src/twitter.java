b17a16d71f2c5b71b7d14c30ef48e8db6a96b3f9
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-20 13:57:49 -0500

    1

--- src/twitter.java
+++ src/twitter.java
@@ -42,64 +42,64 @@ public class twitter {
     	{
     	System.out.println(outputData.toString());
     	}
-    
     	in.close();
     }
     
     private static void convertInitialtoFinalList() {
-	// TODO Auto-generated method stub
-    for(String line : initialList) {
-    		String[] datas = line.split(", *");
-    		intermediateList.add(new Data(findDates(datas[0].trim()), new Description(datas[1].trim(), Integer.parseInt(datas[2].trim()))));
+	    for(String line : initialList) {
+	    		String[] datas = line.split(", *");
+	    		intermediateList.add(new Data(findDates(datas[0].trim()), new Description(datas[1].trim(), Integer.parseInt(datas[2].trim()))));
+	    }
+	    
+	    ListComparator listComparator = new ListComparator();
+	    	sortList(listComparator);
+	
+	    	if (intermediateList.size() <= 1)
+	    	{
+	    	return;
+	    	}
+	    	OutputData last = new OutputData(intermediateList.get(0));
+	    	for(int i = 1; i < intermediateList.size(); i ++)
+	    	{
+		    	Data current = intermediateList.get(i);
+		    	if(last.getDate().isInSameMonth(current.getDate()))
+		    	{
+		    		last.addDescription(current.getDescription());
+		    	}
+		    	else
+		    	{
+			    	outputDatas.add(last);
+			    	last = new OutputData(current);
+		    	}
+	    	}
+	    	outputDatas.add(last);
     }
-    
-    ListComparator listComparator = new ListComparator();
-    	Collections.sort(intermediateList, listComparator);
 
-    	if (intermediateList.size() <= 1)
-    	{
-    	return;
-    	}
-    	List<Data> tempList = new ArrayList<>();
-    	OutputData last = new OutputData(intermediateList.get(0));
-    	for(int i = 1; i < intermediateList.size(); i ++)
-    	{
-    		Data current = intermediateList.get(i);
-    	if
-    	(last.getDate().isInSameMonth(current.getDate()))
-    	{
-    	last.addDescription(current.getDescription());
-    	}
-    	else
-    	{
-    	outputDatas.add(last);
-    	last = new OutputData(current);
-    	}
-    	}
-    	outputDatas.add(last);
-}
+	private static void sortList(ListComparator listComparator) {
+		Collections.sort(intermediateList, listComparator);
+	}
 
 	private static void checkData(String line) {
-    	 String[] data = line.split(", *");
-    	 Date curDate = findDates(data[0].trim());
-    	 if(curDate.compareTo(startDate) <= 0 && curDate.compareTo(endDate) > 0) {
-    		initialList.add(line);
-    	 }	
+		String[] data = line.split(", *");
+		Date curDate = findDates(data[0].trim());
+    	 	if(curDate.compareTo(startDate) <= 0 && curDate.compareTo(endDate) > 0) {
+    	 		initialList.add(line);
+    	 	}	
 	}
 
 	private static Date findDates(String dates)
     {
-    Date retDate = null;
-    String[] date = dates.split("-");
-    if (date.length==2)
-    {
-    		retDate = new Date(Integer.parseInt(date[0].trim()), Integer.parseInt(date[1].trim()), 1);
-    }
-    else if (date.length==3)
-    {
-    		retDate = new Date(Integer.parseInt(date[0].trim()),Integer.parseInt(date[1].trim()),Integer.parseInt(date[2].trim()));
-    }
-    return retDate;
+	    Date retDate = null;
+	    String[] date = dates.split("-");
+	    if (date.length==2)
+	    {
+	    		retDate = new Date(Integer.parseInt(date[0].trim()), Integer.parseInt(date[1].trim()), 1);
+	    }
+	    else if (date.length==3)
+	    {
+	    		retDate = new Date(Integer.parseInt(date[0].trim()),Integer.parseInt(date[1].trim()),Integer.parseInt(date[2].trim()));
+	    }
+	    return retDate;
     }
    
 }
