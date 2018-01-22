21c2ed9d0a4ebef32ee5e791efcc0ff5eb7c3dda
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-21 20:29:52 -0500

    Final

--- src/leet110.java
+++ src/leet110.java
@@ -1,11 +1,54 @@
+//Author : Issac Koshy Panicker
+//21-01-2018
+
 import java.io.*;
 import java.util.*;
 
 class Solution110{
     public boolean isBalanced(TreeNode root) {
-		return false;
-        
-    }
+
+    	if(root==null ) return true;  	
+    	return maxDepth(root)!=-1;
+
+//  Another method with O(n) DFS used
+    	
+//    		int m = maxDepth(root.left);
+//    		int n = maxDepth(root.right);
+//    		
+//    	if(Math.abs(m-n)<=1 && isBalanced(root.left) && isBalanced(root.right) )
+//    		return true;
+//    	
+//	 	return false;
+//    }
+//
+//	private int maxDepth(TreeNode root) {
+//		// TODO Auto-generated method stub
+//		if(root == null)
+//			return 0;
+//		
+//		int m = maxDepth(root.left);
+//		int n = maxDepth(root.right);
+//		
+//		return 1+Math.max(m, n);
+	}
+
+	private int maxDepth(TreeNode root) {
+		
+		if(root == null)
+			return 0;
+		
+		int left = maxDepth(root.left);
+		if(left == -1) return -1;
+		
+		int right = maxDepth(root.right);
+		if(right == -1) return -1;
+		
+		if(Math.abs(left-right)>1) return -1;
+		
+		return 1+Math.max(left, right);
+	}
+    
+    
 }
 
 public class leet110 {
