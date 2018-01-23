00f8af1f65960a5cbae3a3c0a4cceaf74620f92d
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-23 18:09:55 -0500

    Final

---
+++ src/leet172.java
@@ -0,0 +1,38 @@
+//Author : Issac Koshy Panicker
+//23-01-2018
+
+import java.io.*;
+import java.util.*;
+
+class Solution172 {
+    public int trailingZeroes(int n) {
+    	 if(n==0) return 0;
+        
+    	int k = (int) Math.floor(Math.log(n)/Math.log(5));
+    	
+    	int no =0,t=5;
+    	while(k!=0) {
+    		no+= Math.floor(n/t);
+    		t*=5;
+    		k--;
+    	}
+    	
+    	return no;
+    }
+}
+
+public class leet172 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            
+            int ret = new Solution172().trailingZeroes(n);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
