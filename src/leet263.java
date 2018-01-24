b32bde127fa9f038d23d8621107e7f4f8264ce8d
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-23 21:13:20 -0500

    Final

--- src/leet263.java
+++ src/leet263.java
@@ -6,17 +6,22 @@ import java.io.*;
 class Solution263 {
     public boolean isUgly(int num) {
     	
-      	if(num <=0) return false;
-    	if(num ==1) return true;
+    	for (int i=2; i<6 && num>0; i++)
+    	    while (num % i == 0)
+    	        num /= i;
+    	return num == 1;
     	
-    if(num%2==0)
-    	 	return isUgly(num/2);
-    if(num%3==0)
-	 	return isUgly(num/3);
-    if(num%5==0)
-	 	return isUgly(num/5);
-    	
-		return false;
+//      	if(num <=0) return false;
+//    	if(num ==1) return true;
+//    	
+//    if(num%2==0)
+//    	 	return isUgly(num/2);
+//    if(num%3==0)
+//	 	return isUgly(num/3);
+//    if(num%5==0)
+//	 	return isUgly(num/5);
+//    	
+//		return false;
         
     }
 }
