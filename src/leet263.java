947239b170958028e8c54ebe0002eb62720ffa81
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-23 21:09:28 -0500

    Final

---
+++ src/leet263.java
@@ -0,0 +1,42 @@
+//Author : Issac Koshy Panicker
+//23-01-2018
+
+import java.io.*;
+
+class Solution263 {
+    public boolean isUgly(int num) {
+    	
+      	if(num <=0) return false;
+    	if(num ==1) return true;
+    	
+    if(num%2==0)
+    	 	return isUgly(num/2);
+    if(num%3==0)
+	 	return isUgly(num/3);
+    if(num%5==0)
+	 	return isUgly(num/5);
+    	
+		return false;
+        
+    }
+}
+
+public class leet263 {
+    public static String booleanToString(boolean input) {
+        return input ? "True" : "False";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int num = Integer.parseInt(line);
+            
+            boolean ret = new Solution263().isUgly(num);
+            
+            String out = booleanToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
