0cf1c64f9cd71ae2fda1bb07d3972b778584e011
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-25 18:44:24 -0500

    Final

---
+++ src/leet371.java
@@ -0,0 +1,30 @@
+//Author: Issac Koshy Panicker
+//25-01-2018
+
+import java.io.*;
+
+class Solution371 {
+    public int getSum(int a, int b) {
+        
+      return b==0? a : getSum(a^b, (a&b)<<1);
+   
+    }
+}
+
+public class leet371 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int a = Integer.parseInt(line);
+            line = in.readLine();
+            int b = Integer.parseInt(line);
+            
+            int ret = new Solution371().getSum(a, b);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
