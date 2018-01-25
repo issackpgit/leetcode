d5a583eae2f9008d8aafc7141002346b8af4ad28
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-24 19:49:49 -0500

    Initial

---
+++ src/leet345.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+class Solution345 {
+    public String reverseVowels(String s) {
+        
+    	
+    	return null;
+    }
+}
+
+public class leet345 {
+    public static String stringToString(String input) {
+        assert input.length() >= 2;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < input.length() - 1; i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == '\\') {
+                char nextChar = input.charAt(i+1);
+                switch (nextChar) {
+                    case '\"': sb.append('\"'); break;
+                    case '/' : sb.append('/'); break;
+                    case '\\': sb.append('\\'); break;
+                    case 'b' : sb.append('\b'); break;
+                    case 'f' : sb.append('\f'); break;
+                    case 'r' : sb.append('\r'); break;
+                    case 'n' : sb.append('\n'); break;
+                    case 't' : sb.append('\t'); break;
+                    default: break;
+                }
+                i++;
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String s = stringToString(line);
+            
+            String ret = new Solution345().reverseVowels(s);
+            
+            String out = (ret);
+            
+            System.out.print(out);
+        }
+    }
+}
