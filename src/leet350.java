fe041d4a3d4138a844a44d38962655318cf339b3
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-25 21:11:23 -0500

    Final

--- src/leet350.java
+++ src/leet350.java
@@ -1,19 +1,26 @@
+//Author: Issac Koshy Panicker
+//25-01-2018
+
 import java.io.*;
 import java.util.*;
 
 class Solution350 {
     public int[] intersect(int[] nums1, int[] nums2) {
     
-    	Set<Integer> set1 = new HashSet<Integer>();
+    	Map<Integer,Integer> map = new HashMap<Integer,Integer>();
     	ArrayList<Integer> set2 = new ArrayList<Integer>();
     
+   
     	for(int i =0;i<nums1.length;i++) {
-    		set1.add(nums1[i]);
+    		map.put(nums1[i], map.getOrDefault(nums1[i], 0)+1);
     	}
     	for(int i =0;i<nums2.length;i++) {
-    		if(set1.contains(nums2[i]))
+    		if(map.containsKey(nums2[i]) && map.get(nums2[i])>0) {
     			set2.add(nums2[i]);
+    			map.put(nums2[i], map.get(nums2[i])-1);
+    		}
     	}
+    		
     	
     	int[] arr = new int[set2.size()];
 
