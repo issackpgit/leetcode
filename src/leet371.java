9ec6bd7703fc4e19208400a7a8d4b1313231e70a
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-26 23:09:32 -0500

    Final

--- src/leet371.java
+++ src/leet371.java
@@ -5,9 +5,8 @@ import java.io.*;
 
 class Solution371 {
     public int getSum(int a, int b) {
-        
-      return b==0? a : getSum(a^b, (a&b)<<1);
-   
+       
+      return b==0? a : getSum(a^b, (a&b)<<1);  
     }
 }
 
