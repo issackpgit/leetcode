9198cfdea054c431562e37880a8178cf7ebf0794
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-27 17:10:42 -0500

    Initial

---
+++ src/leet415.java
@@ -0,0 +1,53 @@
+import java.io.*;
+
+class Solution415 {
+    public String addStrings(String num1, String num2) {
+        
+        return Integer.toString(Integer.parseInt(num1)+Integer.parseInt(num2));
+        
+    }
+}
+
+public class leet415 {
+    public static String stringToString(String input) {
+        assert input.length() >= 2;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < input.length() - 1; i++) {
+            char currentChar = input.charAt(i);
+            if (currentChar == '\\') {
+                char nextChar = input.charAt(i+1);
+                switch (nextChar) {
+                    case '\"': sb.append('\"'); break;
+                    case '/' : sb.append('/'); break;
+                    case '\\': sb.append('\\'); break;
+                    case 'b' : sb.append('\b'); break;
+                    case 'f' : sb.append('\f'); break;
+                    case 'r' : sb.append('\r'); break;
+                    case 'n' : sb.append('\n'); break;
+                    case 't' : sb.append('\t'); break;
+                    default: break;
+                }
+                i++;
+            } else {
+                sb.append(currentChar);
+            }
+        }
+        return sb.toString();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            String num1 = stringToString(line);
+            line = in.readLine();
+            String num2 = stringToString(line);
+            
+            String ret = new Solution415().addStrings(num1, num2);
+            
+            String out = (ret);
+            
+            System.out.print(out);
+        }
+    }
+}
