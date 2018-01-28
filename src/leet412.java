c93ffbe44f9927d4450756e22e1d2bb793dce626
issackoshypanicker <issac_koshy@yahoo.in>
2018-01-27 23:34:21 -0500

    Initial

---
+++ src/leet412.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution412 {
+    public List<String> fizzBuzz(int n) {
+    	
+    	List<String> list = new ArrayList<>();
+    	
+    	for(int i =1; i <=n;i++) {
+    		if(i%3==0 && i%5==0) {
+    			list.add("FizzBuzz");
+    			continue;
+    		}
+    		if(i%3==0) {
+    			list.add("Fizz");
+    			continue;
+    		}
+    		if(i%5==0) {
+    			list.add("Buzz");
+    			continue;
+    		}
+    		list.add(Integer.toString(i));
+    	}
+    	
+		return list;
+        
+    }
+}
+
+public class leet412{
+	 public static void main(String[] args) throws IOException {
+	        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	        int n =Integer.parseInt(in.readLine());
+	        List<String> list = new Solution412().fizzBuzz(n);
+	        for(String item :list) {
+	        	System.out.println(" "+item+" ");
+	        }
+	 }
+}
