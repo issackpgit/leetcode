1de17fdb347d90b23f58ffc553c98aa0508b47ba
issackpgit <issac_koshy@yahoo.in>
2018-02-09 00:04:32 -0500

    Initial

---
+++ src/leet441.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class Solution441 {
+    public int arrangeCoins(int n) {
+        
+    	return 0;
+    }
+}
+
+public class leet441 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+            
+            int ret = new Solution441().arrangeCoins(n);
+            
+            String out = String.valueOf(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
