75fc482bd27722c7b87b95f38d0e53a08a59ce1b
issackpgit <issac_koshy@yahoo.in>
2018-02-16 17:53:05 -0500

    Simple

--- src/hackerrank/hack4.java
+++ src/hackerrank/hack4.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 
 public class hack4 {
 	
+	
 public static void main(String[] args) throws IOException{
 		
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
