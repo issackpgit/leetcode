1b56a015c66125231ecc04ad9f440b621de07b35
issackpgit <issac_koshy@yahoo.in>
2018-05-30 20:30:49 -0400

    BruteForce

--- src/leet15.java
+++ src/leet15.java
@@ -6,9 +6,37 @@ import java.util.*;
 
 class Solution15 {
     public List<List<Integer>> threeSum(int[] nums) {
-        
+    		
+    	List<List<Integer>> list = new ArrayList<List<Integer>>();
     	
-    	return null;
+    	boolean flag = false;
+    	int n = nums.length;
+    	for(int i =0;i<n-2;i++)
+    	{
+    		for(int j = i+1;j<n-1;j++) {
+    			for(int k =j+1;k<n;k++) {
+    				if(nums[i]+nums[j]+nums[k]==0) {
+    					List<Integer> newList = new ArrayList<Integer>();
+    					newList.add(nums[i]);
+    					newList.add(nums[j]);
+    					newList.add(nums[k]);
+    					Collections.sort(newList);
+    					
+    					for(List item : list) {
+    						if(newList.equals(item)) {
+    							flag =true;
+    						}
+    					}
+    					if(!flag)
+    						list.add(newList);
+    					flag = false;
+    				}
+    			}
+    		}
+    	}
+    		
+    	
+    	return list;
     }
 }
 
