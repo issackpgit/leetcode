258e68052a455c56f74fcd5b8bf98effe93fe0b2
issackpgit <issac_koshy@yahoo.in>
2018-06-20 16:26:28 -0400

    Final

--- src/leet763.java
+++ src/leet763.java
@@ -7,7 +7,40 @@ import java.util.*;
 class Solution763 {
     public List<Integer> partitionLabels(String S) {
     	
-    	return null;
+    		int max =0;
+    		int t =0;
+    		Map<Character, Integer> map = new HashMap<Character, Integer>();
+    		for(int i =0;i<S.length();i++) {
+    			map.put(S.charAt(i),i);
+    		}
+
+    		LinkedList<Integer> list = new LinkedList<Integer>();
+    		max = map.get(S.charAt(0));
+    		for(int i =1;i<S.length();i++) {
+    			
+	    			if(map.get(S.charAt(i))>max) {
+	    				if(i<max) {
+	    					max = map.get(S.charAt(i));
+	    				}
+	    				else {
+	    					if(t==0)
+	    						list.add(i);
+	    					else
+	    						list.add(i - list.get(t-1));
+	    					max = map.get(S.charAt(i));
+	    					t++;
+	    				}
+	    				
+	    			}
+    			
+    		}
+    		int sum =0;
+    		 for (int i: list) {
+    		        sum += i;
+    		    }
+    		list.add(max+1 - sum);
+    	
+    		return list;
         
     }
 }
