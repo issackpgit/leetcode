8aea25765dfa24a48e00d79aa585ee37a0e1a324
issackpgit <issac_koshy@yahoo.in>
2018-06-20 11:58:25 -0400

    Final

--- src/leet43.java
+++ src/leet43.java
@@ -6,18 +6,23 @@ import java.util.*;
 
 class Solution43 {
     public String multiply(String num1, String num2) {
-    	
+    	int c = 0;
     	int[] arr = new int[num1.length()+num2.length()];
     		for(int i = num1.length()-1;i>=0;i--) {
     			for(int j =num2.length()-1;j>=0;j--) {
+    				int temp = Integer.parseInt(String.valueOf(num1.charAt(i))) * Integer.parseInt(String.valueOf(num2.charAt(j)))  ;
+    				int t = temp + arr[i+j+1];
     				
-    				
-    				
+    				arr[i+j] += t / 10;
+    				arr[i+j+1] = t % 10;
+  
     			}
     		}
     	
-    	
-    	return "";
+    		StringBuilder sb = new StringBuilder();
+    	    for(int p : arr) if(!(sb.length() == 0 && p == 0)) sb.append(p);
+    	    return sb.length() == 0 ? "0" : sb.toString();
+//    	    	return Arrays.toString(arr);
     }
 }
 
