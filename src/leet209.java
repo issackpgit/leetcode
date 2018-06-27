65bf9ce2a52bbc243dbb366470e1a536fcc591c7
issackpgit <issac_koshy@yahoo.in>
2018-06-27 15:22:33 -0400

    Final

--- src/leet209.java
+++ src/leet209.java
@@ -7,10 +7,23 @@ import java.util.*;
 class Solution209 {
     public int minSubArrayLen(int s, int[] nums) {
         
-    	int left =0;
+    		int left =0;
+    		int sum =0;
+    		int ans = Integer.MAX_VALUE;
+    		
+    		for(int i =0;i<nums.length;i++) {
+    			sum+=nums[i];
+    			
+    			while(sum>=s) {
+    				ans = Math.min(ans, i-left+1);
+    				sum -= nums[left];
+    				left++;
+    			}
+    		}
     	
     	
-    	return 0;
+    		return (ans != Integer.MAX_VALUE) ? ans : 0;
+    		
     }
 }
 
