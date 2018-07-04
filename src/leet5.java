cd98abb40a7b38e521678bc657f5b32b731bc231
issackpgit <issac_koshy@yahoo.in>
2018-07-04 13:33:12 -0400

    Final

--- src/leet5.java
+++ src/leet5.java
@@ -7,7 +7,24 @@ import java.util.*;
 class Solution5 {
     public String longestPalindrome(String s) {
         
-    	return null;
+    		boolean dp[][] = new boolean[s.length()][s.length()];
+    		
+    		String ans = "";
+    		int n = s.length();
+    		
+    		for(int i = n-1;i>=0;i--) {
+    			for(int j = i;j<n;j++) {
+    				
+    				dp[i][j] = s.charAt(i)==s.charAt(j) && (j-i < 3 || dp[i+1][j-1]);
+    				
+    				if(dp[i][j] && j-i+1 > ans.length()) {
+    					ans = s.substring(i, j+1);
+    				}
+    			}
+    			
+    		}
+    	
+    	return ans;
     }
 }
 
