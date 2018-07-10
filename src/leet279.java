bb9eb2e41d0e1fe76297fce292422eac0b5be866
issackpgit <issac_koshy@yahoo.in>
2018-07-10 16:31:55 -0400

    Final

--- src/leet279.java
+++ src/leet279.java
@@ -6,8 +6,22 @@ import java.util.*;
 
 class Solution279 {
     public int numSquares(int n) {
+    	
+    	int dp[] = new int[n+1];
+    	Arrays.fill(dp, Integer.MAX_VALUE);
+    	
+    	dp[0] = 0;
+    	
+    	for(int i =1;i<=n;i++) {
+        	int min = Integer.MAX_VALUE;
+    		for(int j = 1; i-j*j>=0; j++) {
+    			min = Math.min(dp[i-j*j]+1, min);
+    		}
+    		dp[i] = min;
+    	}
+
         
-    	return 0;
+    	return dp[n];
     }
 }
 
