ceb60e18c32d4048cc397150b02cab9fccd5f986
issackpgit <issac_koshy@yahoo.in>
2018-07-18 16:07:23 -0400

    Final

--- src/leet290.java
+++ src/leet290.java
@@ -5,12 +5,30 @@ import java.io.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 class Solution290 {
     public boolean wordPattern(String pattern, String str) {
-        
-        
-        return false;
+    	String[] arr= str.split(" ");
+        HashMap<Character, String> map = new HashMap<Character, String>();
+        if(arr.length!= pattern.length())
+            return false;
+        for(int i=0; i<arr.length; i++){
+        		char c = pattern.charAt(i);
+        		if(map.containsKey(c)) {
+        			if(map.get(c).equals(arr[i])) {
+        				continue;
+        			}else {
+        				return false;
+        			}
+        		}
+        		else {
+        			if(map.containsValue(arr[i]))
+        				return false;
+        			map.put(c, arr[i]);
+        		}
+        }
+        return true;
     }
 }
 
