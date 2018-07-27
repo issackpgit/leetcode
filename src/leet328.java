f059344f826d9759aa3f429edfcb31f9e4d3e43c
issackpgit <issac_koshy@yahoo.in>
2018-07-26 22:55:27 -0400

    Final

--- src/leet328.java
+++ src/leet328.java
@@ -6,7 +6,20 @@ import java.util.*;
 
 class Solution328 {
     public ListNode oddEvenList(ListNode head) {
-        return null;
+    		if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        
+        while(even!=null && even.next!=null) {
+        		odd.next = even.next;
+        		odd = odd.next;
+        		even.next = odd.next;
+        		even = even.next;
+        }
+        
+        odd.next = evenHead;
+        
+        
+        return head;
     }
 }
 
