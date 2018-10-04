91e4de97adf8659caa6892381bd179311f50723b
issackpgit <issac_koshy@yahoo.in>
2018-10-04 02:11:58 -0400

    Chanegs

--- src/practice/DisjointSet.java
+++ src/practice/DisjointSet.java
@@ -26,12 +26,10 @@ public class DisjointSet {
 	
 	public Node findSet(Node node) {
 		
-		Node parent = node.parent;
-		if(parent == node) {
-			return parent;
+		if(node.parent == node){
+			return node.parent;
 		}
-		
-		node.parent = findSet(node.parent);	
+		node.parent = findSet(node.parent);
 		return node.parent;
 	}
 	
