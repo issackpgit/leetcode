4bb16301054e41583b4ffafccbac3938a84f958c
issackpgit <issac_koshy@yahoo.in>
2018-10-08 18:21:43 -0400

    Depth of tree

---
+++ src/practice/DepthofTree.java
@@ -0,0 +1,40 @@
+package practice;
+
+
+class Node{
+	int data;
+	Node left, right;
+	
+	public Node(int item) {
+		data = item;
+		left=null;
+		right = null;
+	}
+}
+
+public class DepthofTree {
+
+	public static void main(String args[]) {
+		DepthofTree d = new DepthofTree();
+		
+		Node root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.left.left.left = new Node(6);
+		root.left.right.left = new Node(7);
+		
+		int n = d.maxDepth(root);
+		System.out.println(n);
+	}
+
+	private int maxDepth(Node root) {
+		if(root==null)
+			return 0;
+		else {
+			int n = Math.max(maxDepth(root.left), maxDepth(root.right)) +1;
+			return n;
+		}
+	}
+}
