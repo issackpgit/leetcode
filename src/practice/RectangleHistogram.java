810c3a7c81e5bbf310eb960ae376e19b6494a5dc
issackpgit <issac_koshy@yahoo.in>
2018-10-19 21:42:34 -0400

    1

---
+++ src/practice/RectangleHistogram.java
@@ -0,0 +1,53 @@
+package practice;
+
+public class RectangleHistogram {
+	public static void main(String args[]) {
+		RectangleHistogram r = new RectangleHistogram();
+		int a[] = {2,3,1,4,3,3};
+		int n = r.findHist(a);
+		System.out.println(n);
+	}
+
+	private int findHist(int[] a) {
+		Stack<Integer> s = new Stack<Integer>();
+		
+		int i =0,area = 0, max = -1;
+		
+		int n = a.length;
+		while(i<n) {
+			
+			if(s.isEmpty() || a[s.peek()]<=a[i]) {
+				s.push(i);
+			}
+			else if(!s.isEmpty() && a[s.peek()]>a[i]) {
+				while(!s.isEmpty() && a[s.peek()]>a[i]) {
+					int top = s.pop();
+					if(s.isEmpty()) {
+						area = a[top] * i;
+					}
+					else {
+						area = a[top] * (i-s.peek()-1);
+					}
+					max = Math.max(max, area);
+				}
+				s.push(i);
+			}
+			
+			i++;
+			
+		}			
+			while(!s.isEmpty()) {
+				int top = s.pop();
+				if(s.isEmpty()) {
+					area = a[top] * i;
+				}
+				else {
+					area = a[top] * (i-s.peek()-1);
+				}
+				max = Math.max(max, area);
+			}
+
+		
+		return max;
+	}
+}
