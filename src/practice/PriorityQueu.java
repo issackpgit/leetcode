120a56aa840753fa25db5bd4f2dee295d1898f71
issackpgit <issac_koshy@yahoo.in>
2018-10-26 18:55:42 -0400

    2

---
+++ src/practice/PriorityQueu.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.*;
+
+public class PriorityQueu {
+
+	public static void main(String args[]) {
+		PriorityQueue<Integer> i = new PriorityQueue<>();
+		PriorityQueue<String> s = new PriorityQueue<>();
+		PriorityQueu p =new PriorityQueu();
+		
+		i.add(2);
+		i.add(1);
+		i.add(4);
+		i.add(5);
+		i.add(3);
+		
+		s.add("A");
+		s.add("C");
+		s.add("B");
+		s.add("D");
+		s.add("E");
+		
+//		print(s);
+//		p.print(i);
+		for(String item: s) {
+			System.out.println(item);
+		}
+		
+	}
+}
