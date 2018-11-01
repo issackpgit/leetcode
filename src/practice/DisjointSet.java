831472f01a69e68d2c2e3562335627aa5d10ce8b
issackpgit <issac_koshy@yahoo.in>
2018-11-01 14:35:47 -0400

    2

--- src/practice/DisjointSet.java
+++ src/practice/DisjointSet.java
@@ -89,7 +89,6 @@ class DSU {
     int[] rank;
 
     public DSU(int size) {
-    	
         parent = new int[size];
         for (int i = 0; i < size; i++) parent[i] = i;
         rank = new int[size];
